066cb17f72b4f3ca4b56e6586919635dc01f7cd2
Mario Triana <mariotriana1978@gmail.com>
2024-09-14 11:53:17 -0500

    Routes bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { UserProvider } from './context/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserProvider>
-    <BrowserRouter>
+    <BrowserRouter  basename="/dalana_kids_client">
       <App />
     </BrowserRouter>
   </UserProvider>,
